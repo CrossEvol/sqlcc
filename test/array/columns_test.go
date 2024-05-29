b4690c244618fc9316d90ff97b8acff4fd1cd7c3
创圣大天使Evol <296662402@qq.com>
2024-05-29 15:52:42 +0800

    diff for []*Type and []Type

---
+++ test/array/columns_test.go
@@ -0,0 +1,56 @@
+package array
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/crossevol/sqlcc/internal/common"
+	"log"
+	"strconv"
+	"testing"
+)
+
+func TestColumnChangeValueWithoutPointer(t *testing.T) {
+	var columns []common.ColumnPair
+	for i := 0; i < 5; i++ {
+		columns = append(columns, common.ColumnPair{ColumnName: "item" + strconv.Itoa(i)})
+	}
+	s := toJson(columns)
+	fmt.Println(s)
+
+	fmt.Println("-------------------------------------------")
+
+	for _, column := range columns {
+		column.ColumnType = "string"
+		column.IsAutoIncrement = true
+		column.IsPrimaryKey = true
+	}
+	s = toJson(columns)
+	fmt.Println(s)
+}
+
+func TestColumnChangeValueWithPointer(t *testing.T) {
+	var columns []*common.ColumnPair
+	for i := 0; i < 5; i++ {
+		columns = append(columns, &common.ColumnPair{ColumnName: "item" + strconv.Itoa(i)})
+	}
+	s := toJson(columns)
+	fmt.Println(s)
+
+	fmt.Println("-------------------------------------------")
+
+	for _, column := range columns {
+		column.ColumnType = "string"
+		column.IsAutoIncrement = true
+		column.IsPrimaryKey = true
+	}
+	s = toJson(columns)
+	fmt.Println(s)
+}
+
+func toJson(v any) string {
+	bytes, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(bytes)
+}
