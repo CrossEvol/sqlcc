aeebb9bd715bf6e2f5730de5a39f088b03b8eeef
创圣大天使Evol <296662402@qq.com>
2024-05-29 17:12:16 +0800

    for instance , set the default pk column as ID in postgres

--- internal/util/meta_util.go
+++ internal/util/meta_util.go
@@ -13,14 +13,21 @@ func IsNotID(columnPair common.ColumnPair) bool {
 	return !columnPair.IsAutoIncrement && !columnPair.IsPrimaryKey
 }
 
+// DeterminePK TODO: should know about how to determine the column with primary key in postgres
 func DeterminePK(tableMeta *common.TableMeta) {
 	// determine the primary key
 	// when pk column is not integer, it will appear in the position[0]
 	// or if the columnNames contains "id" , it should be named "id"
 	tableMeta.PkColumnName = tableMeta.Columns[0].ColumnName
+	tableMeta.Columns[0].IsAutoIncrement = true
+	tableMeta.Columns[0].IsPrimaryKey = true
 	for _, column := range tableMeta.Columns {
 		if column.ColumnName == "id" {
 			tableMeta.PkColumnName = "id"
+			tableMeta.Columns[0].IsAutoIncrement = false
+			tableMeta.Columns[0].IsPrimaryKey = false
+			column.IsAutoIncrement = true
+			column.IsPrimaryKey = true
 			break
 		}
 	}
